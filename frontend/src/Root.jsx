3b498087e0504a8120f9291d2cef10ed96d8a6ee
liangtam <tammieliang@hotmail.com>
2024-05-30 23:05:00 -0700

    feat: routes set up and pages barrel export setup

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, UserProfile } from './pages';
 
 const Root = () => {
     return (
         <BrowserRouter>
-
+        <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/users/:id" element={<UserProfile/>}/>
+        </Routes>
         </BrowserRouter>
     )
 }
