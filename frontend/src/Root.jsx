001d19a00a654e993d254fadaddb2cb76470c454
abbyhong8 <abbyhong0811@gmail.com>
2024-06-07 02:02:30 -0700

    feat: putting components together for main page

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, UserProfile } from './pages';
+import { Home, UserProfile, SearchRecipe } from './pages';
 
 const Root = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/home" element={<SearchRecipe/>}></Route>
             <Route exact path="/users/:id" element={<UserProfile/>}/>
         </Routes>
         </BrowserRouter>
