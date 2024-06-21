b58b86884c7d0f9aca7c3cb47838a2cb32036ae1
abbyhong8 <abbyhong0811@gmail.com>
2024-06-21 01:56:53 -0700

    fix: edit dummy recipe

--- frontend/src/constants/dummyData.js
+++ frontend/src/constants/dummyData.js
@@ -3,8 +3,8 @@ export const dummyRecipe1 = {
   name: "Pizza",
   image:
     "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
-  ingredients: ["salt", "pepper"],
-  instructions: ["put pizza in the oven"],
+  ingredients: ["salt", "pepper", "flour"],
+  instructions: ["put pizza in the oven", "Baking for 40 mins"],
 };
 
 export const dummyRecipe2 = {
@@ -13,7 +13,7 @@ export const dummyRecipe2 = {
   image:
     "https://www.eatingwell.com/thmb/rmLlvSjdnJCCy_7iqqj3x7XS72c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chopped-power-salad-with-chicken-0ad93f1931524a679c0f8854d74e6e57.jpg",
   ingredients: ["salt", "pepper", "chicken"],
-  instructions: ["defrost chicken, boil water"],
+  instructions: ["defrost chicken", "boil water"],
 };
 
 export const dummyUser = {
