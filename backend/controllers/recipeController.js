189b735ba3af995a898c720c6be580316df4ffc8
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-07-01 19:07:01 -0700

    add delete recipe endpoint to backend

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,3 +1,5 @@
+const Recipe = require("../models/recipeModel");
+
 const handleGetRecipe = (req, res) => {
     // TODO: implement
     res.status(200).json({message: `Successfully retreived recipe`});
@@ -8,10 +10,22 @@ const handleCreateRecipe = (req, res) => {
     res.status(200).json({message: `Successfully saved recipe`});
 }
 
-const handleDeleteRecipe = (req, res) => {
-    // TODO: implement
-    res.status(200).json({message: `Successfully deleted recipe`});
-}
+const handleDeleteRecipe = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await Recipe.findByIdAndDelete(id);
+
+        if (!result) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+
+        return res.status(200).json({ message: 'Successfully deleted recipe' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 
 const handleUpdateRecipe = (req, res) => {
     // TODO: implement
