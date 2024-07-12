310850d8964cbfae274de3f4533c8e1562cc0d69
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-07-11 18:44:43 -0700

    privacy policy and terms of service (AI generated)

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,35 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, UserProfile, LandingPage } from "./pages";
-import { useSelector } from "react-redux";
-import { Navbar } from "./components/";
-import { ShowSignInContextProvider } from "./components/context/ShowSignInContext";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home, LandingPage, UserProfile} from "./pages";
+import {useSelector} from "react-redux";
+import {Navbar} from "./components/";
+import {ShowSignInContextProvider} from "./components/context/ShowSignInContext";
+import TermsOfService from "./pages/legal/TermsOfService";
+import PrivacyPolicy from "./pages/legal/PrivacyPolicy";
 
 const Root = () => {
-  const user = useSelector((state) => state.user.value);
+    const user = useSelector((state) => state.user.value);
 
-  return (
-    <BrowserRouter>
-      <ShowSignInContextProvider>
-        <Navbar />
-      </ShowSignInContextProvider>
-
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
+    return (
+        <BrowserRouter>
             <ShowSignInContextProvider>
-            <LandingPage />
+                <Navbar/>
             </ShowSignInContextProvider>
-          }
-        />
-        <Route exact path="/users/:id" element={<UserProfile />} />
-        <Route exact path="/users/:id" element={<UserProfile />} />
 
-        <Route exact path="/home" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        <ShowSignInContextProvider>
+                            <LandingPage/>
+                        </ShowSignInContextProvider>
+                    }
+                />
+                <Route exact path="/users/:id" element={<UserProfile/>}/>
+                <Route exact path="/users/:id" element={<UserProfile/>}/>
+
+                <Route exact path="/home" element={<Home/>}></Route>
+                <Route exact path="/terms-of-service" element={<TermsOfService/>}></Route>
+                <Route exact path="/privacy-policy" element={<PrivacyPolicy/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default Root;
