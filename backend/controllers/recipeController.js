dd512bba8248013c6e3aaef9af97922f4aa22e14
liangtam <tammieliang@hotmail.com>
2024-07-13 00:50:19 -0700

    fix delete recipe endpt

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -43,7 +43,16 @@ const handleDeleteRecipe = async (req, res) => {
             return res.status(404).json({ message: 'Recipe not found' });
         }
 
-        return res.status(200).json({ message: 'Successfully deleted recipe' });
+        const user = await User.findById(result.userId);
+        
+        if (!user) {
+            return res.status(404).json({error: "User not found."});
+        }
+
+        user.recipes = user.recipes.filter(recipeId => recipeId.toString() !== id);
+        await user.save();
+
+        return res.status(200).json({ message: 'Successfully deleted recipe!' });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Server error', error: error.message });
