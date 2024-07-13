ea4cb1aaf790726bc37311641a088295d6634b78
liangtam <tammieliang@hotmail.com>
2024-07-12 19:50:32 -0700

    move middleware file

---
+++ frontend/src/components/SavedRecipePopup/SavedRecipePopup.jsx
@@ -0,0 +1,32 @@
+import styles from "./SavedRecipePopup.module.css";
+const SavedRecipePopup = ({ recipe, onClose }) => {
+  return (
+    <dialog className={styles.popup}>
+      <div className="flex-col gap-3">
+        <h3>{recipe.name}</h3>
+        <img src={recipe.image} />
+        <div className="flex-col gap-2 align-items-start">
+          <h3>Ingredients</h3>
+          {recipe &&
+            recipe.ingredients.map((ingredient) => {
+              return <div>{ingredient}</div>;
+            })}
+        </div>
+        <div className="flex-col gap-2 align-items-start">
+          <h3>Instructions</h3>
+          {recipe &&
+            recipe.ingredients.map((ingredient) => {
+              return <div>{ingredient}</div>;
+            })}
+        </div>
+        <div className={styles.popupButtons}>
+          <button onClick={onClose} className="pad-2">
+            Close
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default SavedRecipePopup;
