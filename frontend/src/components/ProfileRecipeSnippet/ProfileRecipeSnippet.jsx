e94875404fa6b91f8753ce7c0036fa0d64105798
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-07-18 19:09:20 -0700

    fixed each child in a list should have a unique prop warning

--- frontend/src/components/ProfileRecipeSnippet/ProfileRecipeSnippet.jsx
+++ frontend/src/components/ProfileRecipeSnippet/ProfileRecipeSnippet.jsx
@@ -6,75 +6,81 @@ import { deleteUserRecipe } from "../../redux/users/userSlice";
 import { useDispatch } from "react-redux";
 
 const ProfileRecipeSnippet = ({ fetchingData, setFetchingData, recipe }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const setFallback = (e) => {
-    e.target.src = defaultRecipeImage;
-  };
+    const setFallback = (e) => {
+        e.target.src = defaultRecipeImage;
+    };
 
-  const handleDelete = async () => {
-    try {
-      const response = await api.delete('/recipes/' + recipe._id);
-      if (response.status === 200) {
-          alert('Successfully Deleted');
-          setFetchingData(!fetchingData);
-          dispatch(deleteUserRecipe(recipe._id))
-      } else {
-          alert(response.data.error);
-      }
-  } catch (error) {
-      if (error.response) {
-          if (error.response.status >= 400 && error.response.status < 500) {
-              alert(error.response.data.error || 'An error occurred.');
-          } else {
-              alert('Internal Server Error');
-          }
-      } else {
-          alert('Network error or server is not reachable.');
-      }
-  }
-  };
+    const handleDelete = async () => {
+        try {
+            const response = await api.delete('/recipes/' + recipe._id);
+            if (response.status === 200) {
+                alert('Successfully Deleted');
+                setFetchingData(!fetchingData);
+                dispatch(deleteUserRecipe(recipe._id));
+            } else {
+                alert(response.data.error);
+            }
+        } catch (error) {
+            if (error.response) {
+                if (error.response.status >= 400 && error.response.status < 500) {
+                    alert(error.response.data.error || 'An error occurred.');
+                } else {
+                    alert('Internal Server Error');
+                }
+            } else {
+                alert('Network error or server is not reachable.');
+            }
+        }
+    };
 
+    return (
+        <div className={styles.container}>
+            <div>
+                <h3 className="padB-2 font-weight-700" style={{ textAlign: "left" }}>
+                    {recipe && recipe.name}
+                </h3>
 
-  return (
-    <div className={styles.container}>
-      <div>
-        <h3 className="padB-2 font-weight-700" style={{ textAlign: "left" }}>
-          {recipe && recipe.name}
-        </h3>
+                <div className="profile flex-row gap-3 w-100">
+                    {recipe && (
+                        <img
+                            className={styles.image}
+                            src={recipe.image ? recipe.image : defaultRecipeImage}
+                            onError={setFallback}
+                            alt={recipe.name}
+                        />
+                    )}
+                    <div className={`${styles.ingredients}`}>
+                        <b className="padB-2">Ingredients</b>
+                        {recipe &&
+                            recipe.ingredients.map((ingredient, index) => (
+                                <div key={index}>{ingredient}</div>
+                            ))}
+                    </div>
+                    <div className={`${styles.ingredients}`}>
+                        <b className="padB-2">Instructions</b>
+                        {recipe &&
+                            recipe.instructions.map((instruction, index) => (
+                                <div key={index}>{instruction}</div>
+                            ))}
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="h-1 bg-base-1000 marY-3 w-100"></div>
 
-        <div className="profile flex-row gap-3 w-100">
-          {recipe && <img className={styles.image} src={recipe.image ? recipe.image : defaultRecipeImage} onError={setFallback} alt={recipe.name} />}
-          <div className={`${styles.ingredients}`}>
-            <b className="padB-2">Ingredients</b>
-            {recipe &&
-              recipe.ingredients.map((ingredient) => {
-                return <div>{ingredient}</div>;
-              })}
-          </div>
-          <div className={`${styles.ingredients}`}>
-            <b className="padB-2">Instructions</b>
-            {recipe &&
-              recipe.instructions.map((instruction) => {
-                return <div>{instruction}</div>;
-              })}
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="h-1 bg-base-1000 marY-3 w-100"></div>
-
-        <div className="flex-row w-100 gap-2" style={{justifyContent: 'flex-end'}}>
-        {/* <button className={styles.editBtn} disabled>
+                <div className="flex-row w-100 gap-2" style={{ justifyContent: 'flex-end' }}>
+                    {/* <button className={styles.editBtn} disabled>
             Edit
           </button> */}
-          <button className={styles.deleteBtn} onClick={handleDelete}>
-            Delete
-          </button>
+                    <button className={styles.deleteBtn} onClick={handleDelete}>
+                        Delete
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProfileRecipeSnippet;
