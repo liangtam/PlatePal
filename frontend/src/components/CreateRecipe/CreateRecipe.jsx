d05a715a6a9361bc79bcd4f1d7a2793585af4135
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-07-19 18:45:58 -0700

    changed add recipe to list of inputs

--- frontend/src/components/CreateRecipe/CreateRecipe.jsx
+++ frontend/src/components/CreateRecipe/CreateRecipe.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {useParams} from "react-router-dom";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import {
     Button,
     ChakraProvider,
@@ -13,23 +13,28 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
-    Textarea,
-    useDisclosure
+    IconButton,
+    HStack,
+    VStack, useDisclosure
 } from "@chakra-ui/react";
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
 import api from "../../api";
 
-const CreateRecipe = ({fetchingData, setFetchingData}) => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+const CreateRecipe = ({ fetchingData, setFetchingData }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const [name, setName] = useState("");
-    const [ingredients, setIngredients] = useState("");
-    const [instructions, setInstructions] = useState("");
+    const [ingredients, setIngredients] = useState([""]);
+    const [instructions, setInstructions] = useState([""]);
     const [image, setImage] = useState("");
-    const userId = useParams('userId').userId;
+    const {userId} = useParams();
 
     const handleCreate = async () => {
+        const filteredIngredients = ingredients.filter(item => item.trim() !== "");
+        const filteredInstructions = instructions.filter(item => item.trim() !== "");
+
         try {
             const response = await api.post('/recipes/',
-                {name, ingredients, instructions, image, userId},
+                { name, ingredients: filteredIngredients, instructions: filteredInstructions, image, userId },
                 {
                     headers: {
                         'auth-token': localStorage.getItem('authToken')
@@ -55,24 +60,39 @@ const CreateRecipe = ({fetchingData, setFetchingData}) => {
             }
         }
         setName("");
-        setIngredients("");
-        setInstructions("");
+        setIngredients([""]);
+        setInstructions([""]);
+    };
+
+    const handleInputChange = (index, value, setter, state) => {
+        const newState = [...state];
+        newState[index] = value;
+        setter(newState);
+    };
+
+    const addField = (setter, state) => {
+        setter([...state, ""]);
+    };
+
+    const removeField = (index, setter, state) => {
+        if (state.length > 1) {
+            const newState = state.filter((_, i) => i !== index);
+            setter(newState);
+        }
     };
 
     return (
         <>
-
             <ChakraProvider>
-                <Button onClick={onOpen} w="100%" p={3} my={5} border="1px solid grey" background="none"
-                        borderRadius="25px" disabled>
+                <Button onClick={onOpen} w="100%" p={3} my={5} border="1px solid grey" borderRadius="25px">
                     Create Recipe
                 </Button>
 
                 <Modal isOpen={isOpen} onClose={onClose}>
-                    <ModalOverlay/>
+                    <ModalOverlay />
                     <ModalContent className="modal-content">
                         <ModalHeader className="modal-header">Create Recipe</ModalHeader>
-                        <ModalCloseButton/>
+                        <ModalCloseButton />
                         <ModalBody>
                             <FormControl isRequired>
                                 <FormLabel>Recipe Name</FormLabel>
@@ -85,20 +105,48 @@ const CreateRecipe = ({fetchingData, setFetchingData}) => {
 
                             <FormControl mt={4} isRequired>
                                 <FormLabel>Ingredients</FormLabel>
-                                <Textarea
-                                    placeholder="Enter each ingredient on a new line"
-                                    value={ingredients}
-                                    onChange={(e) => setIngredients(e.target.value)}
-                                />
+                                <VStack spacing={2}>
+                                    {ingredients.map((ingredient, index) => (
+                                        <HStack key={index} spacing={2} width="100%">
+                                            <Input
+                                                placeholder={`Ingredient ${index + 1}`}
+                                                value={ingredient}
+                                                onChange={(e) => handleInputChange(index, e.target.value, setIngredients, ingredients)}
+                                            />
+                                            <IconButton
+                                                icon={<MinusIcon />}
+                                                onClick={() => removeField(index, setIngredients, ingredients)}
+                                                isDisabled={ingredients.length === 1}
+                                                aria-label={"Remove Ingredient"}/>
+                                        </HStack>
+                                    ))}
+                                    <Button onClick={() => addField(setIngredients, ingredients)} leftIcon={<AddIcon />}>
+                                        Add Ingredient
+                                    </Button>
+                                </VStack>
                             </FormControl>
 
                             <FormControl mt={4} isRequired>
                                 <FormLabel>Instructions</FormLabel>
-                                <Textarea
-                                    placeholder="Enter each instruction on a new line"
-                                    value={instructions}
-                                    onChange={(e) => setInstructions(e.target.value)}
-                                />
+                                <VStack spacing={2}>
+                                    {instructions.map((instruction, index) => (
+                                        <HStack key={index} spacing={2} width="100%">
+                                            <Input
+                                                placeholder={`Instruction ${index + 1}`}
+                                                value={instruction}
+                                                onChange={(e) => handleInputChange(index, e.target.value, setInstructions, instructions)}
+                                            />
+                                            <IconButton
+                                                icon={<MinusIcon />}
+                                                onClick={() => removeField(index, setInstructions, instructions)}
+                                                isDisabled={instructions.length === 1}
+                                                aria-label={'Remove Instruction'}/>
+                                        </HStack>
+                                    ))}
+                                    <Button onClick={() => addField(setInstructions, instructions)} leftIcon={<AddIcon />}>
+                                        Add Instruction
+                                    </Button>
+                                </VStack>
                             </FormControl>
 
                             <FormControl mt={4}>
